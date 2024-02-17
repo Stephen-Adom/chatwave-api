31fde8dc057d281c36b6ff751b941bc13f1ee486
Stephen-Adom <stephen.alaska94@hotmail.com>
2024-02-17 14:05:21 +0000

    initial commit

---
+++ src/main/java/com/chats/chatwave/ChatwaveApiApplication.java
@@ -0,0 +1,13 @@
+package com.chats.chatwave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatwaveApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatwaveApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chats/chatwave/ChatwaveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chats.chatwave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatwaveApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
